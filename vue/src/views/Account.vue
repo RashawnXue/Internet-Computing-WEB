8995bac15d03f525f8dcf5f8c719b9b5b01a0b1e
Mas <mas233@icloud.com>
2022-12-04 16:56:40 +0800

    working on me.vue

--- vue/src/views/Account.vue
+++ vue/src/views/Account.vue
@@ -1,4 +1,26 @@
 <script setup>
-    
+    import {ref} from 'vue'
+
 
 </script>
+
+<template>
+    <div class="accountBox">
+        <image src="../assets/img/me.png"></image>
+    </div>
+</template>
+<style>
+body{
+    margin:0;
+    padding:0;
+}
+.accountBox{
+    box-sizing: border-box;
+    width:50%;
+    margin:70px auto;
+    border-radius: 18px;
+    box-shadow: 0 0 10px 10px rgba(25, 128, 139, 0.82);
+    display:flex;
+    flex-direction:row; 
+}
+</style>
