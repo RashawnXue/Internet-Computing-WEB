3864f1161bccf49f605a96fcf618f9862ddffa50
Rashawn <211250142@smail.nju.edu.cn>
2022-12-20 18:54:19 +0800

    后端findContribByUsername、modifyPasswd接口实现

---
+++ springboot/src/test/java/com/web/springboot/controller/UserHandlerTest.java
@@ -0,0 +1,27 @@
+package com.web.springboot.controller;
+
+import com.web.springboot.entity.User;
+import com.web.springboot.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserHandlerTest {
+    @Autowired
+    UserHandler userHandler = new UserHandler();
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void modifyPasswdTest(){
+        User user = new User();
+        user.setUsername("114");
+        user.setPassword("1145145");
+        System.out.println(userHandler.modifyPasswd(user));
+    }
+
+}
