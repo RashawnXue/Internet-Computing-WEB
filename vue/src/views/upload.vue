7fda8a63ea10a5c39d0e77fd1592843a7e415097
wqw <2254133810@qq.com>
2022-12-27 17:35:40 +0800

    已经更新detail页面修改与upload样式修改(细节优化)

--- vue/src/views/upload.vue
+++ vue/src/views/upload.vue
@@ -1,79 +1,5 @@
-<template>
-  <div style="z-index: 0; display: flex; flex-direction: column; align-items: center;">
-    <el-card class="table-container">
-      <div>
-          <el-col :span="12" style="margin-bottom: 20px">
-            <div class="sub-title" >资源名</div>
-            <el-input style="width: 250px" v-model="loadFileParams.name" clearable placeholder="请输入资源名" size="large"></el-input>
-          </el-col>
-
-        <el-row>
-          <el-col :span="12">
-            <div class="sub-title">课程名称</div>
-            <el-select style="width: 250px" v-model="loadFileParams.coursename" class="m-2" placeholder="Select" size="large">
-              <el-option
-                  v-for="item in courses"
-                  :key="item.id"
-                  :label="item.coursename"
-                  :value="item.coursename"
-              />
-            </el-select>
-          </el-col>
-          <el-col :span="12">
-            <div class="sub-title">资源类型</div>
-            <el-select style="width: 250px" v-model="loadFileParams.type" class="m-2" placeholder="Select" size="large">
-              <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-              />
-            </el-select>
-          </el-col>
-        </el-row>
-        <el-form-item  prop="child" v-if="loadFileParams.type == '链接'">
-          <div class="sub-title">请输入您要上传的链接</div>
-          <el-input v-model="loadFileParams.interlinking" clearable placeholder="请输入链接" size="large"></el-input>
-        </el-form-item>
-        <el-row style="margin-bottom: 30px">
-          <div class="sub-title" >资源介绍</div>
-          <el-input
-              type="textarea"
-              :autosize="{ minRows: 2, maxRows: 4}"
-              placeholder="请输入资源介绍"
-              clearable
-              v-model="loadFileParams.intro">
-          </el-input>
-        </el-row>
-
-      </div>
-      <el-upload
-          class="upload-demo"
-          drag
-          :action="uploadFileURL"
-          :data="loadFileParams"
-          :before-upload="uploadBefore"
-          :on-success="successResave"
-          :headers="headers"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :file-list="fileList"
-          multiple
-          v-if="loadFileParams.type == '文件'">
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">贡献你的资源吧</div>
-      </el-upload>
-      <el-button  size="normal" type="success" @click="submitUpload" v-if="loadFileParams.type == '链接'">将信息上传到服务器</el-button>
-    </el-card>
-
-
-  </div>
-
-
-</template>
 <script setup>
-import { Service, CircleCheck,DCaret} from "@element-plus/icons-vue";
+import {HelpFilled, Link, Reading, Notebook, ChatDotSquare,Tools, Service, CircleCheck,DCaret} from "@element-plus/icons-vue";
 </script>
 <script>
 // 后端课程信息数据获取
@@ -106,9 +32,9 @@ export default {
   data() {
     return {
       uploadFileURL:
-          URL.uploadFile,
+      URL.uploadFile,
       uploadLinkURL:
-          URL.uploadLink,// 上传文件的地址!
+      URL.uploadLink,// 上传文件的地址!
       loadFileParams: { // 上传文件的参数！
         name : '',
         coursename : '',
@@ -234,14 +160,86 @@ export default {
     },
     handleSelect(item) {
       if(this.loadFileParams.type == "链接")
-          console.log(item);
+        console.log(item);
     }
   },
-
-
-
 }
 </script>
+
+<template>
+  <div style="z-index: 0; display: flex; flex-direction: column; align-items: center;">
+    <el-card class="table-container">
+      <div>
+          <el-col :span="12" style="margin-bottom: 20px">
+            <div class="sub-title" ><el-icon style="vertical-align: -0.2em;" size="large"><Reading /></el-icon>资源名</div>
+            <el-input style="width: 250px" v-model="loadFileParams.name" clearable placeholder="请输入资源名" size="large"></el-input>
+          </el-col>
+
+        <el-row>
+          <el-col :span="12">
+            <div class="sub-title"><el-icon style="vertical-align: -0.2em;" size="large"><Notebook /></el-icon>课程名称</div>
+            <el-select style="width: 250px" v-model="loadFileParams.coursename" class="m-2" placeholder="Select" size="large">
+              <el-option
+                  v-for="item in this.courses"
+                  :key="item.id"
+                  :label="item.coursename"
+                  :value="item.coursename"
+              />
+            </el-select>
+          </el-col>
+          <el-col :span="12">
+            <div class="sub-title"><el-icon style="vertical-align: -0.2em;" size="large"><HelpFilled /></el-icon>资源类型</div>
+            <el-select style="width: 250px" v-model="loadFileParams.type" class="m-2" placeholder="Select" size="large">
+              <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+              />
+            </el-select>
+          </el-col>
+        </el-row>
+        <el-form-item  prop="child" v-if="loadFileParams.type == '链接'" style="margin-bottom: 10px">
+          <div class="sub-title"><el-icon style="vertical-align: -0.2em;" size="large"><Link /></el-icon>链接</div>
+          <el-input v-model="loadFileParams.interlinking" clearable placeholder="请输入链接" size="large"></el-input>
+        </el-form-item>
+        <el-row style="margin-bottom: 30px">
+          <div class="sub-title" ><el-icon style="vertical-align: -0.2em;" size="large"><ChatDotSquare /></el-icon>资源介绍</div>
+          <el-input
+              type="textarea"
+              :autosize="{ minRows: 2, maxRows: 4}"
+              placeholder="请输入资源介绍"
+              clearable
+              v-model="loadFileParams.intro">
+          </el-input>
+        </el-row>
+
+      </div>
+      <el-upload
+          class="upload-demo"
+          drag
+          :action="uploadFileURL"
+          :data="loadFileParams"
+          :before-upload="uploadBefore"
+          :on-success="successResave"
+          :headers="headers"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :file-list="fileList"
+          multiple
+          v-if="loadFileParams.type == '文件'">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">贡献你的资源吧</div>
+      </el-upload>
+      <el-button  size="normal" type="success" @click="submitUpload" v-if="loadFileParams.type == '链接'">将信息上传到服务器</el-button>
+    </el-card>
+
+
+  </div>
+
+
+</template>
 
 <style>
 
