7625890548ad7860021659234e2e0a12d41cb4c9
Rashawn <211250142@smail.nju.edu.cn>
2022-12-29 14:56:27 +0800

    更改细节

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// 1. 定义路由组件.
-// 也可以从其他文件导入
+
+// 定义路由组件.
 import Home from '../views/Home.vue'
 import Search from '../views/Search.vue'
 import detail from '../views/Detail.vue'
@@ -11,9 +11,8 @@ import upload from '../views/Upload.vue'
 import Class from '../views/Class.vue'
 
 
-// 2. 定义一些路由
+// 定义一些路由
 // 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
 const routes = [
     { path: '/', component: Home },
     { path: '/search', component: Search },
@@ -25,11 +24,9 @@ const routes = [
     { path: '/class', component: Class },
 ]
 
-// 3. 创建路由实例并传递 `routes` 配置
-// 你可以在这里输入更多的配置，但我们在这里
-// 暂时保持简单
+// 创建路由实例并传递 `routes` 配置
 const Router = createRouter({
-    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    // 使用 hash 模式。
     history: createWebHashHistory(),
     routes, // `routes: routes` 的缩写
 })
